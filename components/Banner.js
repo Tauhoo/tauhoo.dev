a4978c1c9363d4b811705437405df6559d93478e
Tauhoo <tauhoo_ice@hotmail.com>
2021-02-25 00:58:11 +0700

    set up banner

---
+++ components/Banner.js
@@ -0,0 +1,45 @@
+import React from "react"
+import styled from "styled-components"
+import Topic from "./Topic"
+import Text from "./Text"
+
+const Container = styled.div`
+  width: 100%;
+  height: 350px;
+  padding: 50px 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  div:nth-child(1) {
+    display: none;
+  }
+  @media (max-width: 840px) {
+    div:nth-child(1) {
+      display: block;
+    }
+    div:nth-child(2) {
+      display: none;
+    }
+    height: 300px;
+  }
+`
+
+function Banner({ darkMode = false, style }) {
+  return (
+    <Container style={style}>
+      <Topic style={{ textAlign: "center" }} level={1}>
+        Wellcom !!
+      </Topic>
+      <Topic style={{ textAlign: "center" }} level={2}>
+        Wellcom !!
+      </Topic>
+      <br></br>
+      <Text style={{ textAlign: "center" }}>
+        I’m wachirawit wacharak. This is my web blogs. Let’s enjoy it.
+      </Text>
+    </Container>
+  )
+}
+
+export default Banner
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ import Head from "next/head"
 import Container from "../components/Container"
 import Text from "../components/Text"
 import Topic from "../components/Topic"
+import Banner from "../components/Banner"
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Tauhoo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Banner></Banner>
       <Container>
         <Text>ice is the best ไอซ์</Text>
         <Text bold highlight>
